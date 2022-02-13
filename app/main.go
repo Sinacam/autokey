51e5bc1d2c049cd11336b6f9e63ad2e4e92c5b00
Sinacam <stinkingmadgod@gmail.com>
2022-02-13 22:48:38 +0800

    rewrote main

--- app/main.go
+++ app/main.go
@@ -2,24 +2,13 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/Sinacam/autokey"
-	"gopkg.in/yaml.v2"
 )
 
 func main() {
-	f, err := os.Open("input.yml")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var yml interface{}
-	err = yaml.NewDecoder(f).Decode(&yml)
-	if err != nil {
-		fmt.Println(err)
-		return
+	yml := map[interface{}]interface{}{
+		"file": "input.yml",
 	}
 
 	expr, err := autokey.Compile(yml)
